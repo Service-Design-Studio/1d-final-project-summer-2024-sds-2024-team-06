93fca42b5d727485da7a287cf399d95d58d52daf
fooory <114894561+fooory@users.noreply.github.com>
2024-06-19 22:33:39 +0800

    updated scenarios

--- features/art_pieces.feature
+++ features/art_pieces.feature
@@ -1,9 +1,24 @@
-Feature: View art pieces
-  As a visitor
-  I want to view a list of art pieces
-  So that I can see all available art pieces
+Feature: Gallery Walk
+  As an adolescent who wants to name my emotions
+  I want to be guided through a process that allows me to notice my subconscious emotions
+  So that I can practice emotional well-being and manage my mental state better
 
-  Scenario: Viewing the list of art pieces
+  Scenario: Participant begins Gallery Walk activity
+    Given participant chosen to partake in the Gallery Walk
+    When I choose a picture from the carousel
+    Then the other pictures should disappear, a mp3 audio player and text box will be displayed
+
+  Scenario: During the activity
+    Given I am listening to the voice-over
+    When I want to express my emotions in writing
+    Then text will be shown in the text box as accordance to user input
+
+  Scenario: User has completed the Gallery Walk activity
+    Given User is done with the gallery walk
+    When they click the end activity button
+    Then the journal entry is automatically saved to their journal tab, browser redirect to journal
+
+  Scenario: Viewing the list of art gallery
     Given there are art pieces in the database
     When I visit the art pieces page
     Then I should see a list of art pieces
--- features/step_definitions/art_pieces_steps.rb
+++ features/step_definitions/art_pieces_steps.rb
@@ -1,7 +1,51 @@
+##Scenario 1: Participant begins Gallery Walk activity
+
+Given('participant chosen to partake in the Gallery walk') do
+
+end
+
+When('I choose a picture from the carousel') do
+
+end
+
+Then('the other pictures should disappear, a mp3 audio player and text box will be displayed') do
+
+end
+
+##Scenario 2: During the Activity
+
+Given('I am listening to the voice-over') do
+
+end
+
+When('I want to express my emotions in writing') do
+  visit art_pieces_path
+end
+
+Then('text will be shown in the text box as accordance to user input') do
+
+end
+
+##Scenario 3: User has completed the Gallery Walk Activity
+
+Given('User is done with the gallery walk') do
+
+end
+
+When('they click the end activity button') do
+  visit art_pieces_path
+end
+
+Then('the journal entry is automatically saved to their journal tab, browser redirect to journal') do
+
+end
+
+##Scenario 4: Viewing the list of art gallery
+
 Given('there are art pieces in the database') do
   @art_pieces = ArtPiece.create([
-    { title: 'Mona Lisa', artist: 'Leonardo da Vinci' },
-    { title: 'Starry Night', artist: 'Vincent van Gogh' }
+    { artID: 123, artvoice: 'voice.com', captions: 'this is the one la sia', artTitle: 'mona lisa', image_url: 'mona.com' },
+    { artID: 124, artvoice: 'yes.com', captions: 'eh nice', artTitle: 'van gogh', image_url: 'gogh.com' }
   ])
 end
 
