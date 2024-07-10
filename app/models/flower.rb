235c67686af657121f511bfabb1cbde1a3a8d47d
Ammaryvs <168526699+Ammaryvs@users.noreply.github.com>
2024-07-10 17:00:56 +0800

    starting with sprint_2 for rspec

--- app/models/flower.rb
+++ app/models/flower.rb
@@ -3,17 +3,17 @@ class Flower < ApplicationRecord
   validates :mood, presence: true
   validates :color, presence: true
   validates :date_created, presence: true
+  
+  before_save :set_day
 
-<<<<<<< HEAD
   def year_created
     flower&.date_created&.year
-=======
-  before_save :set_day
+  end
 
   private
   
   def set_day
     self.day = date_created.yday if date_created.present?
->>>>>>> e0de73e1333a2cbeaeddf3a29ebde2960e076887
   end
 end
+
