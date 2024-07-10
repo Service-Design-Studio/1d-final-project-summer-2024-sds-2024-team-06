39857c6e90ac3e50c8b050d3a58c83ab01c54f52
Jonaslim201 <jonaslimwx@gmail.com>
2024-07-10 13:06:56 +0800

    fixed updateMood

--- db/schema.rb
+++ db/schema.rb
@@ -71,9 +71,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_09_132237) do
     t.string "name"
     t.string "color"
     t.string "hexcode"
-    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -94,4 +94,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_09_132237) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "flowers", "users"
 end
