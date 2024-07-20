72f14030f82c3b702ba2e859988580fc5ca916df
Roziallegro <rozi@LAPTOP-05ESM8S1>
2024-07-20 09:33:18 +0800

    updated routes for journal entry pages

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -12,9 +12,8 @@ import GalleryWalk from '../pages/GalleryWalk';
 import GalleryWalkSession from '../pages/GalleryWalkSession';
 import Journal from '../pages/JournalEntryHistory';
 import JournalEntry from '../pages/JournalEntry';
-import JournalEntryForm from '../pages/JournalEntryForm';
-import UpdateMoodForm from './UpdateMoodForm';
-import { useUser } from '../pages/User';
+import JournalGoalForm from './JournalGoalForm';
+import JournalOpenForm from './JournalOpenForm';
 
 
 // To use dotted paper background: <div style={dottedPaper}></div>
@@ -27,79 +26,7 @@ const dottedPaper = {
 
 // Routing
 const App = () => {
-  const { currentUser } = useUser();
-
-  //fake flower data
-  const flowerData = {
-    mood: 'happy',
-    color: 'yellow',
-    date_created: new Date().toISOString(),
-  };
-
-  //standard colors and emotions everyone starts off with
-  //users cannot add/remove/change the mood name, but they can change the color and hexcode
-  const standard_moods = [
-    { name: 'Excited', color: 'Neon green', hexcode: '#39FF14' },
-    { name: 'Very happy', color: 'Yellow', hexcode: '#FFFF00' },
-    { name: 'Meh', color: 'Bright blue', hexcode: '#007FFF' },
-    { name: 'Tired', color: 'Black', hexcode: '#000000' },
-    { name: 'Content', color: 'Brown', hexcode: '#964B00' },
-    { name: 'Angry', color: 'Red', hexcode: '#FF0000' },
-    { name: 'Happy', color: 'Lime green', hexcode: '#32CD32' },
-    { name: 'In love', color: 'Pink', hexcode: '#FFC0CB' },
-    { name: 'Unhappy', color: 'Navy blue', hexcode: '#000080' },
-    { name: 'Teary', color: 'Light purple', hexcode: '#E6E6FA' },
-    { name: 'Upset', color: 'Dark blue', hexcode: '#00008B' },
-    { name: 'Confused', color: 'Gray', hexcode: '#808080' },
-  ]
   
-  //To get user id => currentUser.id
-  //To check if user is guest => currentUser.guest = true or false
-
-  //function to create a flower for a user
-  function createFlowerForUser(flowerData) {
-    // flowerData.user_id = userId;
-    fetch(`/api/flowers`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ flower: flowerData })
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log('Flower created:', data);
-    })
-    .catch((error) => {
-      console.error('Error creating flower:', error);
-    });
-  }
-  
-
-  //function to create a standard mood for a user
-  function createMoods(moodData) {
-    fetch(`/api/moods`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ mood: moodData })
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log('Mood created:', data);
-    })
-    .catch((error) => {
-      console.error('Error creating mood:', error);
-    });
-  }
-
-  //function to lop through all standard moods to add them to a user
-  function addAllMoodsToCurrentUser(moods) {
-    moods.forEach(mood => {
-      createMoods(mood);
-    });
-  }
   return (
     <div>
       {/* <button onClick={createFlowerForUser(flowerData)}>flower</button>
@@ -115,7 +42,9 @@ const App = () => {
           <Route exact path="/gallery-walk/:id" element={<GalleryWalkSession />}></Route>
           <Route exact path="/journal" element={<Journal />}></Route>
           <Route exact path="/journal/:id" element={<JournalEntry />}></Route>
-          <Route exact path="/journal/create" element={<JournalEntryForm />}></Route>
+          {/* <Route exact path="/journal/create" element={<JournalEntryForm />}></Route> */}
+          <Route exact path="/journal/goal-setting" element={<JournalGoalForm />}></Route>
+          <Route exact path="/journal/open-ended" element={<JournalOpenForm />}></Route>
 
         </Routes>
         
