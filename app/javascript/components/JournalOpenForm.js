f8c3439d49ca4d1eeeedd281e7ecaba4d3dc795d
yingxuan <160840085+Pillowmon@users.noreply.github.com>
2024-07-23 19:11:10 +0800

    added queueManager

--- app/javascript/components/JournalOpenForm.js
+++ app/javascript/components/JournalOpenForm.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { useState } from 'react';
 //import { useUser } from '../pages/User.js'; 
-import { guideMe, generateTip } from '../api/gemini.js'
-
+//import { guideMe, generateTip } from '../api/gemini.js'
 import Navigation from "../components/Navigation";
+import { enqueueGenerateTip, enqueueGuideMe } from '../api/queueManager';
 
 
 // function to dynamically get today's date
@@ -164,7 +164,7 @@ export default function JournalOpenForm() {
                                                 return;
                                               };
                                               // llm to generate a tip with tip title
-                                              const generatedTip = await generateTip(journalEntry);
+                                              const generatedTip = await enqueueGenerateTip(journalEntry);
                                               // post to end-api
                                               let data_id = await createJournalForUser({
                                                 //user_id: currentUser.id,
@@ -198,7 +198,7 @@ export default function JournalOpenForm() {
                                                   setGenerateButton("Generating...");
                                                   disableButton("button-prompt");
                                                   // llm to generate a prompt
-                                                  const generatedGuide = await guideMe(journalEntry);
+                                                  const generatedGuide = await enqueueGuideMe(journalEntry);
                                                   //update the prompt-space with new lines/ break lines
                                                   setTipBody(formatPrompt(generatedGuide.response))
                                                   // change button back to "Guide Me"
